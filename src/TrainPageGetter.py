cb25083e041d00a67d80944b9de3dbf8f4c3a311
Bodo Minea <bodominea@gmail.com>
2019-08-18 00:33:46 +0300

    Int delays on stations, split next station details, group station details in objects, bool is_real_time, int milepost

--- src/TrainPageGetter.py
+++ src/TrainPageGetter.py
@@ -36,15 +36,23 @@ def state_decoder(state):
         'route': info_box[9][1][1][0][0][1],
         'status': info_box[11][1][1][0][0][1],
         'latest_information': {
-        	'stop': info_box[13][1][1][0][0][1].split("[")[0].strip(),
+        	'station': {
+        		'name': info_box[13][1][1][0][0][1].split("[")[0].strip(),
+        		'id': get_station_ID_by_name(info_box[13][1][1][0][0][1].split("[")[0].strip())
+        	},
         	'status': info_box[13][1][1][0][0][1].split("[")[1].strip()[:-1],
         	'time': info_box[15][1][1][0][0][1]
         },
         'delay': None if info_box[17][1][1][0][0][1]=='' else int(info_box[17][1][1][0][0][1]),
         'destination': info_box[19][1][1][0][0][1],
         'arrival_time': info_box[21][1][1][0][0][1],
-        'next_stop': info_box[23][1][1][0][0][1],
-        'next_stop_time': info_box[25][1][1][0][0][1],
+        'next_stop': {
+        	'stop': { 
+        		'name': info_box[23][1][1][0][0][1].strip(),
+        		'id': get_station_ID_by_name(info_box[23][1][1][0][0][1].strip())
+        	},
+        	'time': info_box[25][1][1][0][0][1]
+        },
         'distance': info_box[27][1][1][0][0][1],
         'trip_duration': info_box[29][1][1][0][0][1],
         'average_speed': info_box[31][1][1][0][0][1],
@@ -73,14 +81,16 @@ def state_decoder(state):
             entry = route_info_box[2 * entry_number - 1][1]
 
             entry_data = {
-                'milepost': entry[1][0][0][1],
-                'station': entry[3][0][0][1].strip(),
-                'station_id': get_station_ID_by_name(entry[3][0][0][1].strip()),
+                'milepost': None if entry[1][0][0][1]=='' else int(entry[1][0][0][1]),
+                'station': {
+                	'name': entry[3][0][0][1].strip(),
+                	'id': get_station_ID_by_name(entry[3][0][0][1].strip()),
+                },
                 'arrival_time': entry[5][0][0][1],
-                'stop_duration': entry[7][0][0][1],
+                'stop_duration': None if (entry[7][0][0][1]=='' or entry[7][0][0][1]=='&nbsp;') else int(entry[7][0][0][1]),
                 'departure_time': entry[9][0][0][1],
-                'is_real_time': entry[11][0][0][1],
-                'delay': entry[13][0][0][1],
+                'is_real_time': True if entry[11][0][0][1]=='Real' else False,
+                'delay': None if (entry[13][0][0][1]=='' or entry[13][0][0][1]=='&nbsp;') else int(entry[13][0][0][1]),
                 'mentions': entry[15][0][0][1],
             }
 
@@ -92,7 +102,7 @@ def state_decoder(state):
             route_data.append(entry_data)
 
     return {
-        'departure_date': departure_date,
+        'departure_date': departure_date.split(' ')[2],
         'info_box': info_box_data,
         'route_data': route_data,
     }
